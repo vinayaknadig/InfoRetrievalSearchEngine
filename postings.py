8c29cde8109e9de8d1af2097cb8b71de3f2eadfe
vinayaknadig <vinayaknadig20@gmail.com>
2020-02-22 13:45:44 -0800

    initial class declaration

--- postings.py
+++ postings.py
@@ -8,5 +8,14 @@ class Posting:
         self.doc_id = id;
         self.freq = frequency;
         self.tf_idf_score = tf_id;
+    
+    def getDocId(self):
+        return self.doc_id;
+
+    def getFreq(self):
+        return self.freq;
+
+    def getTfIdfScore(self):
+        return self.tf_idf_score;
 
     
