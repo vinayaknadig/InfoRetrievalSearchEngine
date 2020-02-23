46230c8e54320ccb4a3c2cec733e4d58170d77cd
vinayaknadig <vinayaknadig20@gmail.com>
2020-02-23 01:59:48 -0800

    added needed methods to postings.py; created tokenizer to take content from json files

--- postings.py
+++ postings.py
@@ -1,21 +1,23 @@
 
 class Posting:
-    doc_id;
-    freq;
-    tf_idf_score;
+    doc_id = 0
+    freq = 0
+    tf_idf_score = 0
 
     def __init__(self, id, frequency, tf_id):
-        self.doc_id = id;
-        self.freq = frequency;
-        self.tf_idf_score = tf_id;
+        self.doc_id = id
+        self.freq = frequency
+        self.tf_idf_score = tf_id
     
     def getDocId(self):
-        return self.doc_id;
+        return self.doc_id
 
     def getFreq(self):
-        return self.freq;
+        return self.freq
 
     def getTfIdfScore(self):
-        return self.tf_idf_score;
+        return self.tf_idf_score
 
+    def setTfIdfScore(self, value):
+        self.tf_idf_score = value
     
---
+++ tokenizer.py
@@ -0,0 +1,40 @@
+import json
+import re
+from lxml import etree
+import lxml.html
+
+class Tokenizer:
+    file = ""
+
+    def __init__(self, filename):
+        self.file = filename
+    
+    def tokenize(self) -> list:
+        to_return = []
+        try:
+            json_obj = open(self.file, "r")
+            content = json.load(json_obj);
+            doc = lxml.html.document_fromstring(content["content"])
+            lines = doc.text_content().split()
+            for word in lines:
+                word = word.lower()
+                tokens = re.split('[^a-z0-9]', word)
+                for x in tokens:
+                    if re.match('^[a-z]+$', x) or re.match('^[0-9]+$', x):
+                        to_return.append(x)
+            json_obj.close()
+        except FileNotFoundError:
+            print("File not found. Please try again.")
+        
+        return to_return
+
+    def computeWordFrequencies(tokens) -> {str:int}:
+        to_return = {}
+        already_done = set()
+        for token in tokens:
+            if token in already_done:
+                to_return[token] += 1
+            else:
+                to_return[token] = 1
+                already_done.add(token)
+        return to_return
